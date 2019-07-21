f238bbb38d2697d961e4e3431002ba2c93e59d33
Wladimir88Shevchenko <48687461+Wladimir88Shevchenko@users.noreply.github.com>
2019-07-21 23:12:14 +0300

    Add files via upload

---
+++ game/scenario.js
@@ -0,0 +1,294 @@
+function show() {
+    console.log("Name is " + this.name);
+    console.log("He is a " + this.who);
+    console.log("He has " + this.health + " hp");
+    if (this.who == "warrior") {
+        console.log("His armor: " + this.armor);
+        console.log("He has " + this.strength + " strength");
+    } else {
+        console.log("He has " + this.intellect + " intellect");
+    }
+    console.log("=======================================");
+}
+
+/* MAGE SETTINGS */
+
+function humanoid_mage(name) {
+    this.maxhealth = 100;
+    this.who = "mage";
+    this.name = name;
+    this.health = 100;
+    this.armor = 0;
+    this.intellect = 60;
+}
+
+humanoid_mage.prototype.attack = function(enemy) {
+    if (enemy.armor >= this.intellect) {
+        enemy.armor -= this.intellect;
+    } else {
+        var change = enemy.armor - this.intellect;
+        enemy.armor = 0;
+        enemy.health += change;
+        if (enemy.health <= 0) {
+            alert(enemy.name + "  lose this game.");
+            console.log(enemy.name + "  lose this game.");
+            if (confirm("Хотите сыграть еще?")) {
+                location.reload();
+            } else {
+                window.close();
+            }
+
+        }
+    }
+}
+
+humanoid_mage.prototype.read = function() {
+    this.intellect += 100;
+}
+
+humanoid_mage.prototype.show = show;
+
+/* WARRIOR SETTINGS */
+
+function humanoid_warrior(name) {
+    this.maxhealth = 100;
+    this.who = "warrior";
+    this.name = name;
+    this.health = 100;
+    this.armor = 200;
+    this.strength = 30;
+}
+
+humanoid_warrior.prototype.attack = function(enemy) {
+    if (enemy.armor >= this.strength) {
+        enemy.armor -= this.strength;
+
+    } else {
+        var change = enemy.armor - this.strength;
+        enemy.armor = 0;
+        enemy.health += change;
+        if (enemy.health <= 0) {
+            alert(enemy.name + "  lose this game.");
+            console.log(enemy.name + "  lose this game.");
+            if (confirm("Хотите сыграть еще?")) {
+                location.reload();
+            } else {
+                window.close();
+            }
+        }
+    }
+}
+
+humanoid_warrior.prototype.show = show;
+
+humanoid_warrior.prototype.train = function() {
+    this.health += 90;
+    if (this.health > this.maxhealth) {
+        this.maxhealth = this.health;
+
+    }
+}
+
+/* ZZZ WARRIOR SETTINGS */
+
+/* FIRST PLAYER */
+var first_name = prompt("Как вас зовут?");
+var who_will_be;
+var health1 = document.querySelectorAll("#health1")[0];
+var power1 = document.querySelectorAll("#power1")[0];
+var armor_title1 = document.querySelectorAll("#armor_health1")[0];
+var armor1 = document.querySelectorAll("#armor1")[0];
+var atk1 = document.querySelectorAll("#btn_atk1")[0];
+var train1 = document.querySelectorAll("#btn_train1")[0];
+var l_btns = document.querySelectorAll(".left_buttons>button");
+var r_btns = document.querySelectorAll(".right_buttons>button");
+var proto_line1 = document.querySelectorAll(".proto_line_left")[0];
+var left_health_line = document.querySelectorAll(".health_line_left>.green_line")[0];
+var left_armor_line = document.querySelectorAll(".proto_line_left>.gray_line")[0];
+document.querySelectorAll("#first_player_name")[0].innerHTML = first_name;
+
+do {
+    who_will_be = prompt(first_name + " будете играть за мага или за воина?", "маг");
+} while (who_will_be != "маг" && who_will_be != "воин");
+
+if (who_will_be == "маг") {
+    var first = new humanoid_mage(first_name);
+    var div_left = document.querySelectorAll(".firstCharacter")[0];
+    div_left.src = "images/mage/left/normal.png";
+    power1.innerHTML = first.intellect;
+    health1.innerHTML = first.health;
+    armor1.style.display = "none";
+    armor_title1.style.display = "none";
+    train1.innerHTML = "Читать";
+    proto_line1.style.visibility = "hidden";
+} else {
+    var first = new humanoid_warrior(first_name);
+    var div_left = document.querySelectorAll(".firstCharacter")[0];
+    div_left.src = "images/warrior/left/normal.png";
+    document.querySelectorAll("#power_title1")[0].innerHTML = "Сила: ";
+    power1.innerHTML = first.strength;
+    health1.innerHTML = first.health;
+    armor1.innerHTML = first.armor;
+}
+/* ZZZ FIRST PLAYER */
+
+
+
+/* SECOND PLAYER */
+var second_name = prompt("Как вас зовут?");
+var who_will_be2;
+var health2 = document.querySelectorAll("#health2")[0];
+var power2 = document.querySelectorAll("#power2")[0];
+var armor_title2 = document.querySelectorAll("#armor_health2")[0];
+var armor2 = document.querySelectorAll("#armor2")[0];
+var atk2 = document.querySelectorAll("#btn_atk2")[0];
+var train2 = document.querySelectorAll("#btn_train2")[0];
+var proto_line2 = document.querySelectorAll(".proto_line_right")[0];
+var right_armor_line = document.querySelectorAll(".proto_line_right>.gray_line")[0];
+var right_health_line = document.querySelectorAll(".health_line_right>.green_line")[0];
+document.querySelectorAll("#second_player_name")[0].innerHTML = second_name;
+
+do {
+    who_will_be2 = prompt(second_name + " будете играть за мага или за воина?", "воин");
+} while (who_will_be2 != "маг" && who_will_be2 != "воин");
+
+if (who_will_be2 == "маг") {
+    var second = new humanoid_mage(second_name);
+    var div_right = document.querySelectorAll(".secondCharacter")[0];
+    div_right.src = "images/mage/right/normal.png";
+    power2.innerHTML = second.intellect;
+    health2.innerHTML = second.health;
+    armor2.style.display = "none";
+    armor_title2.style.display = "none";
+    train2.innerHTML = "Читать";
+    proto_line2.style.visibility = "hidden";
+} else {
+    var second = new humanoid_warrior(second_name);
+    var div_right = document.querySelectorAll(".secondCharacter")[0];
+    div_right.src = "images/warrior/right/normal.png";
+    document.querySelectorAll("#power_title2")[0].innerHTML = "Сила: ";
+
+    power2.innerHTML = second.strength;
+    health2.innerHTML = second.health;
+    armor2.innerHTML = second.armor;
+}
+
+/* ZZZ SECOND PLAYER */
+
+
+/* TRAIN */
+
+train1.addEventListener("click", function() {
+    if (first.who == "mage") {
+        div_left.src = "images/mage/left/read.png";
+        setTimeout(function() {
+            div_left.src = "images/mage/left/normal.png";
+        }, 1000);
+        first.read();
+        power1.innerHTML = first.intellect;
+    } else {
+        div_left.src = "images/warrior/left/train.png";
+        setTimeout(function() {
+            div_left.src = "images/warrior/left/normal.png";
+        }, 1000);
+        first.train();
+        health1.innerHTML = first.health;
+        let proc1 = (first.health / first.maxhealth) * 100;
+        left_health_line.style.width = proc1 + "%";
+    }
+    l_btns[0].style.visibility = "hidden";
+    l_btns[1].style.visibility = "hidden";
+    r_btns[0].style.visibility = "visible";
+    r_btns[1].style.visibility = "visible";
+
+});
+
+train2.addEventListener("click", function() {
+
+    if (second.who == "mage") {
+        div_right.src = "images/mage/right/read.png";
+        setTimeout(function() {
+            div_right.src = "images/mage/right/normal.png";
+        }, 1000);
+        second.read();
+        power2.innerHTML = second.intellect;
+    } else {
+        div_right.src = "images/warrior/right/train.png";
+        setTimeout(function() {
+            div_right.src = "images/warrior/right/normal.png";
+        }, 1000);
+        second.train();
+        health2.innerHTML = second.health;
+        let proc2 = (second.health / second.maxhealth) * 100;
+        right_health_line.style.width = proc2 + "%";
+    }
+    l_btns[0].style.visibility = "visible";
+    l_btns[1].style.visibility = "visible";
+    r_btns[0].style.visibility = "hidden";
+    r_btns[1].style.visibility = "hidden";
+});
+
+/* ZZZ TRAIN */
+
+
+
+/* ATACK */
+
+atk1.addEventListener("click", function() {
+
+    if (first.who == "mage") {
+        div_left.src = "images/mage/left/attak.png";
+        setTimeout(function() {
+            div_left.src = "images/mage/left/normal.png";
+        }, 1000);
+    } else {
+        div_left.src = "images/warrior/left/attack.png";
+        setTimeout(function() {
+            div_left.src = "images/warrior/left/normal.png";
+        }, 1000);
+    }
+    first.attack(second);
+    health2.innerHTML = second.health;
+    armor2.innerHTML = second.armor;
+    var proc3 = (second.health / second.maxhealth) * 100;
+    right_health_line.style.width = proc3 + "%";
+    proc3 = (second.armor / 200) * 100;
+    right_armor_line.style.width = proc3 + "%";
+    l_btns[0].style.visibility = "hidden";
+    l_btns[1].style.visibility = "hidden";
+    r_btns[0].style.visibility = "visible";
+    r_btns[1].style.visibility = "visible";
+
+});
+
+atk2.addEventListener("click", function() {
+    if (second.who == "mage") {
+        div_right.src = "images/mage/right/attak.png";
+        setTimeout(function() {
+            div_right.src = "images/mage/right/normal.png";
+        }, 1000);
+    } else {
+        div_right.src = "images/warrior/right/attack.png";
+        setTimeout(function() {
+            div_right.src = "images/warrior/right/normal.png";
+        }, 1000);
+    }
+
+    second.attack(first);
+    health1.innerHTML = first.health;
+    armor1.innerHTML = first.armor;
+
+    proc3 = (first.health / first.maxhealth) * 100;
+    left_health_line.style.width = proc3 + "%";
+    proc3 = (first.armor / 200) * 100;
+    left_armor_line.style.width = proc3 + "%";
+    l_btns[0].style.visibility = "visible";
+    l_btns[1].style.visibility = "visible";
+    r_btns[0].style.visibility = "hidden";
+    r_btns[1].style.visibility = "hidden";
+
+
+});
+
+
+/* ZZZ ATACK */
